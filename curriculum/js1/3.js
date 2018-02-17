eba2104fedceb8eac21a91d6e335e521b64875a0
David Tae <tae@garagescript.org>
2018-02-17 15:34:59 -0800

    solved problem 3

--- curriculum/js1/3.js
+++ curriculum/js1/3.js
@@ -5,8 +5,12 @@
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (x) => {
+  if(x<1) {
+    return "";}
+  if(x===1){
+    return "hello";}
+  return "hello"+solution(x-1);
 };
 
 module.exports = {
